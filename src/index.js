ee8370ec8a9d25f185162a6c2afcda901cecd018
Ngoc Hung <huynhngochung12092001@gmail.com>
2023-06-23 02:49:22 +0700

    Update Cors

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:3000/",
-            "https://book-store-wf6c.onrender.com/",
+            "https://book-store-wf6c.onrender.com",
         ],
     })
 );
