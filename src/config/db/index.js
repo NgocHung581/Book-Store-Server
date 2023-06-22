a9ba44149a40c940fb95781e16b8134a38406a0b
Ngoc Hung <huynhngochung12092001@gmail.com>
2023-06-23 02:22:33 +0700

    Update connect DB

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 async function connectDB() {
     try {
         mongoose.set("strictQuery", false);
-        await mongoose.connect("mongodb://127.0.0.1:27017/book_store");
+        await mongoose.connect(
+            "mongodb+srv://huynhngochung12092001:NgocHungne1209@cluster0.edn3dcd.mongodb.net/"
+        );
         console.log("Database connected successfully");
     } catch (error) {
         console.log("connect failure");
